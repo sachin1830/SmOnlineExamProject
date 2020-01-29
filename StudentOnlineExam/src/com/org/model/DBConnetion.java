e66760108f92da0e080889479c23b3400fc6aef6
sachin1830 <46964628+sachin1830@users.noreply.github.com>
2020-01-29 17:25:04 +0530

    Update DBConnetion.java

--- StudentOnlineExam/src/com/org/model/DBConnetion.java
+++ StudentOnlineExam/src/com/org/model/DBConnetion.java
@@ -2,12 +2,13 @@ package com.org.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.net.URI;
 
 public class DBConnetion {
 
 	public static Connection createConnection() throws Exception
 	   {
-		  Connection con=null;    
+		 Connection con=null;    
 		   URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
 		    String username = jdbUri.getUserInfo().split(":")[0];
 		    String password = jdbUri.getUserInfo().split(":")[1];
