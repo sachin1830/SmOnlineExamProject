1beca04ce1d0e838d8a93729283ab0e2c659d762
sachin1830 <46964628+sachin1830@users.noreply.github.com>
2020-01-29 12:20:27 +0530

    Update DBConnetion.java

--- StudentOnlineExam/src/com/org/model/DBConnetion.java
+++ StudentOnlineExam/src/com/org/model/DBConnetion.java
@@ -7,12 +7,13 @@ public class DBConnetion {
 
 	public static Connection createConnection() throws Exception
 	   {
-		   Connection con=null;    
-		   String url = "jdbc:mysql://localhost:3306/StudentDB";
-		   String username = "root";
-		   String password = "Sachin@1830";
-		   Class.forName("com.mysql.cj.jdbc.Driver");
-		   con = DriverManager.getConnection(url, username, password);
+		  Connection con=null;    
+		   URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+		    String username = jdbUri.getUserInfo().split(":")[0];
+		    String password = jdbUri.getUserInfo().split(":")[1];
+		    String port = String.valueOf(jdbUri.getPort());
+		    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+		    con = DriverManager.getConnection(jdbUrl, username, password);
 		   return con;
 	   }
 
