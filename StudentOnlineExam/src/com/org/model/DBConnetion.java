f0b759aabadcf4f71ff4846c3fd8ff924402e731
sachin1830 <46964628+sachin1830@users.noreply.github.com>
2020-01-30 23:56:03 +0530

    Update DBConnetion.java

--- StudentOnlineExam/src/com/org/model/DBConnetion.java
+++ StudentOnlineExam/src/com/org/model/DBConnetion.java
@@ -2,19 +2,20 @@ package com.org.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.net.URI;
 
 public class DBConnetion {
 
 	public static Connection createConnection() throws Exception
 	   {
-		 Connection con=null;
-		String url = "jdbc:mysql://localhost:3306/registration_database";
-	    String username = "root";
-		String password = "Sachin@1830";
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection(url, username, password);
-		return con;
+		 Connection con=null;    
+		     URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+		    String username = jdbUri.getUserInfo().split(":")[0];
+		    String password = jdbUri.getUserInfo().split(":")[1];
+		    String port = String.valueOf(jdbUri.getPort());
+		    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+		    con = DriverManager.getConnection(jdbUrl, username, password);
+		   return con;
 	   }
 
 }
